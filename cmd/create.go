b774db1c61e66275c1af57299ead907d81e0566c
Pavel Malinov <pmmalinov01@gmail.com>
2021-02-17 12:08:46 +0200

    added create and gitignore

--- cmd/create.go
+++ cmd/create.go
@@ -16,9 +16,22 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
+	"github.com/pmmalinov01/fyrectl/pkg/creds"
 	"github.com/spf13/cobra"
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	createFyreCluster = "https://api.fyre.ibm.com/rest/v1/?operation=build"
 )
 
 // createCmd represents the create command
@@ -33,6 +46,39 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
+		content, err := ioutil.ReadFile("conf.yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		j2, err := yaml.YAMLToJSON(content)
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		createCluster := strings.NewReader(string(j2))
+
+		req, err := http.NewRequest("POST", createFyreCluster, createCluster)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		uCreds, err := creds.GetCreds()
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.SetBasicAuth(uCreds.UserName, uCreds.ApiKey)
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data, _ := ioutil.ReadAll(res.Body)
+		res.Body.Close()
+		var prettyJSON bytes.Buffer
+		error := json.Indent(&prettyJSON, data, "", "\t")
+		if error != nil {
+			log.Println("JSON parse error", error)
+
+		}
+		fmt.Println(string(prettyJSON.Bytes()))
 	},
 }
 
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.0
+	sigs.k8s.io/yaml v1.2.0
 )
